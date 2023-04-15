51900032379f54c147efd4c00d5014cc60cb4798
Андрей <macbook@MacBook-Pro-MacBook.local>
2023-04-15 11:27:50 +0300

    Python4Lab

--- pythonlabs/lab4.py
+++ pythonlabs/lab4.py
@@ -1,10 +1,42 @@
+import numpy as np
 import matplotlib.pyplot as plt
 
-fig = plt.figure()
-print (type(fig))
-plt.scatter(1.0, 1.0)
-print (fig.axes)
+try:
+    with open("data.txt", "r") as f:
+        data = np.loadtxt(f)
+        x_values = data[:, 0]
+        y_values = data[:, 1]
+        print("Данные загружены ")
+except FileNotFoundError:
+    print("Файл не найден. Ручной режим.")
+    x_min = float(input("Введите минимальное значение x: "))
+    x_max = float(input("Введите максимальное значение x: "))
+    step = float(input("Введите шаг вычисления: "))
+
+    def f(x, a, b, c):
+        return a * np.sin(b * x) + c
+
+    a = float(input("Введите значение a: "))
+    b = float(input("Введите значение b: "))
+    c = float(input("Введите значение c: "))
+
+    x_values = np.arange(x_min, x_max + step, step)
+
+    y_values = f(x_values, a, b, c)
+
+    with open("data.txt", "w") as f:
+        for i in range(len(x_values)):
+            f.write("{:.3f}\t{:.3f}\n".format(x_values[i], y_values[i]))
+        print("Данные сохранены в файл data.txt")
+
+fig, ax = plt.subplots()
+
+ax.plot(x_values, y_values)
+
+ax.set_title("График Функции")
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
 
 plt.show()
 
-
+fig.savefig("graph.svg")
